7888dd828fe141c6ec07ffbe52268bad1b0eabca
drunnals313 <davidrunnals@gmail.com>
2018-06-17 18:40:31 -0400

    initial file set up for burger app

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Eat-Da-Burger
-Restaurant app that lets users input the names of burgers they'd like to eat
+# FriendFinder
+*Friend Matching Application Based on Chakra Balance Questions*
+
+Application is deployed to Heroku (https://fathomless-crag-68766.herokuapp.com/)
+
+**Instructions to spin up your own version:**
+* Clone this repository: https://github.com/drunnals313/FriendFinder
+    * git clone: https://github.com/drunnals313/FriendFinder
+    * cd FriendFinder
+* In the Bash Command Terminal Window use the following commands to install the necessary node package manager packages and node modules:
+    * npm init -y
+    * npm i express
+    * npm i body-parser
+    * npm i path
+* In the command terminal, navigate to the folder you cloned this repository's code to.
+* In the command terminal, type "node server" to make sure the application is listening on the correct port.
+
+
+**Chakra Questions:**
+* Question 1: You feel grounded, safe, and secure.
+* Question 2: You are stimulating or fueling your creative passion.
+* Question 3: You find purpose in your work.
+* Question 4: Your personal habits support who you want to be.
+* Question 5: You are open to receiving love.
+* Question 6: You always speak the truth.
+* Question 7: You stand up for the injustices that you see.
+* Question 8: You trust your intuition.
+* Question 9: You feel at peace.
+* Question 10: You take time to recharge yourself.
+
+* Matching Method: Respond to statements/ questions with values from 1 (Strongly Disagree) to 5 (Strongly Agree). When the answers are submitted, the system matches other users with the closest response to determine the best match.
---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+
+var friendsArray = [
+	{
+		name: 'Sophia',
+		photo: 'https://github.com/drunnals313/FriendFinder/blob/master/images/sophia.jpg?raw=true',
+		scores: [
+			'2',
+			'3',
+			'4',
+			'3',
+			'2',
+			'1',
+			'5',
+			'5',
+			'3',
+			'1'
+		]
+	},
+	{
+		name: 'Olivia',
+		photo: 'https://github.com/drunnals313/FriendFinder/blob/master/images/olivia.jpg?raw=true',
+		scores: [
+			'5',
+			'4',
+			'1',
+			'2',
+			'4',
+			'5',
+			'3',
+			'2',
+			'1',
+			'4'
+		]
+	},
+	{
+		name: 'Emma',
+		photo: 'https://github.com/drunnals313/FriendFinder/blob/master/images/emma.jpg?raw=true',
+		scores: [
+			'2',
+			'1',
+			'1',
+			'4',
+			'1',
+			'2',
+			'4',
+			'3',
+			'2',
+			'1'
+		]
+	},
+	{
+		name: 'Ava',
+		photo: 'https://github.com/drunnals313/FriendFinder/blob/master/images/ava.jpg?raw=true',
+		scores: [
+			'5',
+			'4',
+			'3',
+			'1',
+			'2',
+			'3',
+			'4',
+			'3',
+			'2',
+			'4'
+		]
+	},
+	{
+		name: 'Isabella',
+		photo: 'https://github.com/drunnals313/FriendFinder/blob/master/images/isabella.jpg?raw=true',
+		scores: [
+			'2',
+			'3',
+			'4',
+			'4',
+			'5',
+			'5',
+			'3',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Jackson',
+		photo: 'https://github.com/drunnals313/FriendFinder/blob/master/images/jackson.jpg?raw=true',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'4',
+			'3',
+			'4',
+			'5',
+			'5',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Liam',
+		photo: 'https://github.com/drunnals313/FriendFinder/blob/master/images/liam.jpg?raw=true',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Noah',
+		photo: 'https://github.com/drunnals313/FriendFinder/blob/master/images/noah.jpg?raw=true',
+		scores: [
+			'4',
+			'3',
+			'2',
+			'1',
+			'5',
+			'4',
+			'4',
+			'1',
+			'2',
+			'4'
+		]
+	},
+	{
+		name: 'Aiden',
+		photo: 'https://github.com/drunnals313/FriendFinder/blob/master/images/aiden.jpg?raw=true',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'3',
+			'4',
+			'1',
+			'5',
+			'2',
+			'5',
+			'3'
+		]
+	},
+	{
+		name: 'Lucas',
+		photo: 'https://github.com/drunnals313/FriendFinder/blob/master/images/lucas.jpg?raw=true',
+		scores: [
+			'2',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
