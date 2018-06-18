c540a34f065329d82aef335a8527a10948768758
drunnals313 <davidrunnals@gmail.com>
2018-06-18 14:50:29 -0400

    updated css and footer and buttons from bootstrap

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
 
-var port = process.env.PORT || 3030;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
