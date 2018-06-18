50bc7b0f333cffe4e86af5a3d164bdabcabb7b87
drunnals313 <davidrunnals@gmail.com>
2018-06-17 23:28:55 -0400

    updated most files but still trying to decide on sql or sequelize and other key foundations   getting a little ahead of myself

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE `burgers` (
+  `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+  `burger_name` VARCHAR( 255) NOT NULL,
+  `devoured` BOOLEAN( 255 ) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+
+  PRIMARY KEY ( `id` ) 
+);
